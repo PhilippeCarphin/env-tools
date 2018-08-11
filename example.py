02c1fe31c1ecee2cc6782b5f4f93e5711077bf13
Philippe Carphin <philippe.carphin@polymtl.ca>
2018-08-11 15:25:10 -0400

    Renamed everything to pyenv and extracted code using pyenv from code implementing pyenv
    Also added man file

---
+++ example.py
@@ -0,0 +1,104 @@
+import pyenv
+
+'''
+================================================================================
+SSH_CLIENT
+================================================================================
+'''
+@pyenv.parses(['SSH_CLIENT'])
+def process_ssh_client(value):
+    tokens = value.split(' ')
+    return {"ip":tokens[0],
+            "port1": tokens[1],
+            "port2":tokens[2],
+            "rest":"_".join(tokens[3:])}
+
+@pyenv.stringizes(['SSH_CLIENT'])
+@pyenv.pretty_stringizes(['SSH_CLIENT'])
+def pretty_str_ssh_client(var, value):
+    return var + '=' + ' '.join(value[k] for k in value)
+
+
+'''
+================================================================================
+colon list variables
+================================================================================
+'''
+colon_lists = ['CDPATH', 'PATH', 'LD_LIBRARY_PATH', 'DYLD_LIBRARY_PATH']
+@pyenv.parses(colon_lists)
+def process_colon_list(value):
+    return value.strip(':').split(':')
+
+@pyenv.stringizes(colon_lists)
+def colon_list_to_str(var, value):
+    return var + '=' + ':'.join(value)
+
+@pyenv.pretty_stringizes(colon_lists)
+def colon_list_to_pretty_str(var, value):
+    prefix = var + '='
+    joiner = '\n' + ' '*len(prefix)
+    return prefix + joiner.join(value)
+
+@pyenv.compares(colon_lists)
+def compare_lists(before, after):
+    new = set(after) - set(before)
+    gone = set(before) - set(after)
+    kept = set(before).intersection(set(after))
+    indent = '\n      '
+    result = ''
+    if new:
+        result += '    ADDED:' + indent + indent.join(new) + '\n'
+    if (new or gone) and kept:
+        result += '    KEPT:' + indent + indent.join(kept) + '\n'
+    if gone:
+        result += '    DELETED:' + indent + indent.join(gone) + '\n'
+    return result.strip('\n')
+
+'''
+================================================================================
+colon list variables
+================================================================================
+'''
+space_lists = ['SSH_CONNECTION']
+@pyenv.parses(space_lists)
+def process_space_list(value):
+    return value.strip(' ').split(' ')
+
+@pyenv.stringizes(space_lists)
+@pyenv.pretty_stringizes(space_lists)
+def space_list_to_str(var, value):
+    return var + '=' + ' '.join(value)
+
+
+'''
+================================================================================
+================================== MAIN PART ===================================
+
+Take various actions based on command line arguments
+================================================================================
+'''
+
+if __name__ == "__main__":
+    penv = pyenv.PyEnv()
+    import sys
+
+    if len(sys.argv) > 1:
+        command = sys.argv[1]
+        if command == "dump":
+            if len(sys.argv) > 2:
+                with open(sys.argv[2], 'w') as f:
+                    f.write(penv.json_dumps())
+            else:
+                print(penv.json_dumps())
+        elif command == "pretty":
+            print(penv.pretty())
+        elif command == "get":
+            print(penv.get_pretty_str(sys.argv[2]))
+        elif command == 'compare':
+            with open(sys.argv[2], 'r') as f:
+                env_before = pyenv.PyEnv(json.loads(f.read()))
+            with open(sys.argv[3], 'r') as f:
+                env_after = pyenv.PyEnv(json.loads(f.read()))
+            print(compare_envs(env_before, env_after))
+    else:
+        print(penv.pretty())
--- pyenv.py
+++ pyenv.py
@@ -18,7 +18,7 @@ def make_decorator(dictionary):
             return f
     return env_decorator
 
-class Penv:
+class PyEnv:
     ''' Class that encapsulates a dictionnary of environment variables
     The keys are variable names and the values are the parsed string values
     of the environment variables as defined by the 'processor' functions '''
@@ -140,86 +140,8 @@ pretty_stringizes = make_decorator(pretty_stringizers)
 comparers = {}
 compares = make_decorator(comparers)
 
-'''
-================================================================================
-SSH_CLIENT
-================================================================================
-'''
-@parses(['SSH_CLIENT'])
-def process_ssh_client(value):
-    tokens = value.split(' ')
-    return {"ip":tokens[0],
-            "port1": tokens[1],
-            "port2":tokens[2],
-            "rest":"_".join(tokens[3:])}
-
-@stringizes(['SSH_CLIENT'])
-@pretty_stringizes(['SSH_CLIENT'])
-def pretty_str_ssh_client(var, value):
-    return var + '=' + ' '.join(value[k] for k in value)
-
-
-'''
-================================================================================
-colon list variables
-================================================================================
-'''
-colon_lists = ['CDPATH', 'PATH', 'LD_LIBRARY_PATH', 'DYLD_LIBRARY_PATH']
-@parses(colon_lists)
-def process_colon_list(value):
-    return value.strip(':').split(':')
-
-@stringizes(colon_lists)
-def colon_list_to_str(var, value):
-    return var + '=' + ':'.join(value)
-
-@pretty_stringizes(colon_lists)
-def colon_list_to_pretty_str(var, value):
-    prefix = var + '='
-    joiner = '\n' + ' '*len(prefix)
-    return prefix + joiner.join(value)
-
-@compares(colon_lists)
-def compare_lists(before, after):
-    new = set(after) - set(before)
-    gone = set(before) - set(after)
-    kept = set(before).intersection(set(after))
-    indent = '\n      '
-    result = ''
-    if new:
-        result += '    ADDED:' + indent + indent.join(new) + '\n'
-    if (new or gone) and kept:
-        result += '    KEPT:' + indent + indent.join(kept) + '\n'
-    if gone:
-        result += '    DELETED:' + indent + indent.join(gone) + '\n'
-    return result.strip('\n')
-
-'''
-================================================================================
-colon list variables
-================================================================================
-'''
-space_lists = ['SSH_CONNECTION']
-@parses(space_lists)
-def process_space_list(value):
-    return value.strip(' ').split(' ')
-
-@stringizes(space_lists)
-@pretty_stringizes(space_lists)
-def space_list_to_str(var, value):
-    return var + '=' + ' '.join(value)
-
-
-'''
-================================================================================
-================================== MAIN PART ===================================
-
-Take various actions based on command line arguments
-================================================================================
-'''
-print("HELLO FROM PYENV")
 if __name__ == "__main__":
-    penv = Penv()
+    penv = PyEnv()
     import sys
 
     if len(sys.argv) > 1:
@@ -229,16 +151,16 @@ if __name__ == "__main__":
                 with open(sys.argv[2], 'w') as f:
                     f.write(penv.json_dumps())
             else:
-                print(Penv().json_dumps())
+                print(penv.json_dumps())
         elif command == "pretty":
             print(penv.pretty())
         elif command == "get":
-            print(Penv().get_pretty_str(sys.argv[2]))
+            print(penv.get_pretty_str(sys.argv[2]))
         elif command == 'compare':
             with open(sys.argv[2], 'r') as f:
-                env_before = Penv(json.loads(f.read()))
+                env_before = PyEnv(json.loads(f.read()))
             with open(sys.argv[3], 'r') as f:
-                env_after = Penv(json.loads(f.read()))
+                env_after = PyEnv(json.loads(f.read()))
             print(compare_envs(env_before, env_after))
     else:
         print(penv.pretty())
