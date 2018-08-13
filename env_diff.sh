8976b158e94969fa86b327c90e7734cb20d93ef5
Philippe Carphin <philippe.carphin@polymtl.ca>
2018-08-13 15:28:15 +0000

    quick error fix

--- env_diff.sh
+++ env_diff.sh
@@ -11,8 +11,8 @@ done
 this_dir=$(cd -P $(dirname $this_file) > /dev/null && pwd)
 env_before_file=/tmp/$(whoami)_env_before.txt
 env_after_file=/tmp/$(whoami)_env_after.txt
-$this_dir/env_analyser.sh dump > $env_before_file
+$this_dir/pyenv.sh dump > $env_before_file
 eval $@
-$this_dir/env_analyser.sh dump > $env_after_file
+$this_dir/pyenv.sh dump > $env_after_file
 
-$this_dir/env_analyser.sh compare $env_before_file $env_after_file
+$this_dir/pyenv.sh compare $env_before_file $env_after_file
