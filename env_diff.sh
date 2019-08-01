1b0799952efbb150784350824ecb61da3becb141
Philippe Carphin <philippe.carphin@polymtl.ca>
2019-08-01 12:49:42 -0400

    Added quotes to silence PyCharm warnings

--- env_diff.sh
+++ env_diff.sh
@@ -9,26 +9,27 @@ follow_links()
     local file="$1"
     local curr_dir
 
-    while [ -L $file ] ; do
-        curr_dir="$(dirname $file)"
-        file="$(readlink $file)"
-        if is_relative_link $file ; then
+    while [ -L "$file" ] ; do
+        curr_dir="$(dirname "$file")"
+        file="$(readlink "$file")"
+        if is_relative_link "$file" ; then
             file="$curr_dir/$file"
         fi
-        ls -l $file 1>&2
+        ls -l "$file" 1>&2
     done
 
-    echo $file
+    echo "$file"
 }
 
 
-this_dir=$(dirname $(follow_links ${BASH_SOURCE[0]}))
+this_dir=$(dirname "$(follow_links "${BASH_SOURCE[0]}")")
 
 env_before_file=/tmp/$(whoami)_env_before.txt
 env_after_file=/tmp/$(whoami)_env_after.txt
 
-$this_dir/pyenv.sh dump > $env_before_file
+"$this_dir"/pyenv.sh dump > "$env_before_file"
+# shellcheck disable=SC2068
 eval $@
-$this_dir/pyenv.sh dump > $env_after_file
+"$this_dir"/pyenv.sh dump > "$env_after_file"
 
-$this_dir/pyenv.sh compare $env_before_file $env_after_file
+"$this_dir"/pyenv.sh compare "$env_before_file" "$env_after_file"
