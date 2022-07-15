80c89cdd5253131a5a16f387a5e548c3d167ed80
Philippe Carphin <philippe.carphin@polymtl.ca>
2022-07-14 22:17:42 -0400

    Add help command for upcoming manpage

--- libexec/env-tools/ienvtool.py
+++ libexec/env-tools/ienvtool.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python3
 
 import argparse
+import os
 from philenv import *
 from envtool import get_command
 from envdiff import diff_command
 from env_find import find_in_env_command
 from symbolboss import find_symbol_command
 
+p = argparse.ArgumentParser(description="""
+Analyze current computing environment and effect of
+certain commands on the environemnt.  
 
-p = argparse.ArgumentParser(description="Analyze current computing environment and effect of certain commands on the environemnt.  (Run envtool SUBCOMMAND --help for help on subcommands)")
+(Run 'envtool SUBCOMMAND --help' for help on subcommands
+ or 'envtool help SUBCOMMAND' for manual page on SUBCOMMAND) """)
 sp = p.add_subparsers()
 
 diff_parser = sp.add_parser('diff')
@@ -33,13 +38,22 @@ find_symbol_parser.add_argument('--demangle', action='store_true')
 find_symbol_parser.add_argument('--nice', action='store_true')
 find_symbol_parser.set_defaults(command=find_symbol_command)
 
-args = p.parse_args()
+def phil_help_command(args):
+    this_dir = os.path.dirname(os.path.realpath(__file__))
+    os.environ['MANPATH'] = os.path.normpath(os.path.join(this_dir, '..', '..', 'share', 'man'))
+    os.execl('/usr/bin/env', '/usr/bin/env', 'man', f'{args.posarg[0]}')
+
+help_parser = sp.add_parser('help', description="Show manpage for envtool-command")
+help_parser.add_argument('posarg', nargs=1)
+help_parser.set_defaults(command=phil_help_command)
+
 
 def f():
     pass
 
 function = type(f)
 
+args = p.parse_args()
 if not isinstance(args.command, function):
     raise Exception("--func parameter is for internal use only")
 
--- libexec/env-tools/script.sh
+++ libexec/env-tools/script.sh
@@ -1,4 +1,3 @@
 
 export VAR=VALUE
 export PATH=extra_path:$PATH
-echo "PISS BUCKET"
